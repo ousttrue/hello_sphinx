c45de98f3772b0de8414be9ba6ddecff8d635788
ousttrue <ousttrue@gmail.com>
2022-01-11 15:56:55 +0900

    i18n

--- docs/conf.py
+++ docs/conf.py
@@ -51,3 +51,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+language = "ja" # -l 未指定のときのデフォルト
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False     # optional.
--- docs/index.md
+++ docs/index.md
@@ -3,14 +3,14 @@
 % You can adapt this file completely to your liking, but it should at least
 % contain the root `toctree` directive.
 
-# Welcome to hello's documentation!
+# ようこそハロー
 
 ```{toctree}
 :maxdepth: 2
 :caption: Contents
 ```
 
-## Indices and tables
+## 索引と検索
 
 * {ref}`genindex`
 * {ref}`modindex`
