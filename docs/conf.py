fcfafa3055beb4901dd25dc57b889bb191a9bde5
ousttrue <ousttrue@gmail.com>
2022-01-11 15:50:14 +0900

    myst

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'me'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
